0a3ab94611fe5315b93408505be35c5b6ccd39ce
Gabriel Teles <gab.teles@hotmail.com>
2014-05-23 17:30:19 -0300

    Remover Barrinhas de Membro

---
+++ Pimp-My-MRM.css.php
@@ -0,0 +1,14 @@
+<?php
+	// Determina o tipo do conteúdo e o charset
+	header("Content-type: text/css; charset: UTF-8");
+?>
+/*
+ * Pimp My MRM
+ * @version: 20140523
+ */
+ 
+<?php if (isset($_GET['RemoverBarrinhas'])): ?>
+.post-content > .poster > .row:nth-child(3) {
+	display: none;
+}
+<?php endif; ?>
